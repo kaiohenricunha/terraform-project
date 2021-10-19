21399ed105d23ca40174828e8fcabda103e65b76
kaio-cunha <kaio.cunha@hp.com>
2021-10-19 12:05:27 -0300

    first ec2 instance deployed

--- main.tf
+++ main.tf
@@ -1,19 +1,48 @@
--provider "aws" {
+provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "robertom-terraform-ec2-example" {
+resource "aws_instance" "kaiocm-terraform-ec2" {
   ami = "ami-0c2d06d50ce30b442"
   instance_type = "t2.micro"
-  key_name= "robertom-terraform-test"
-  #  subnet_id = "subnet-5f359a38" # PRIVATE-AZ-2A
-  #  subnet_id = "subnet-4a32f303" # PRIVATE-AZ-2B
   subnet_id = "subnet-c2e2cd9a" # PRIVATE-AZ-2C
 
-  vpc_security_group_ids = [aws_security_group.robertom-terraform-sg-example.id]
-  iam_instance_profile = "PingInstanceRole"
-  user_data = file("./scripts/install_nginx.sh")
+  vpc_security_group_ids = [aws_security_group.kaioc-terraform-sg.id]
+  iam_instance_profile = "PingInstanceRole" # has some safety policies from hp
   tags = {
-    Name = "robertom-terraform-ec2-example"
+    Name = "kaiocm-terraform-ec2"
   }
+}
 
+# Security Group
+resource "aws_security_group" "kaioc-terraform-sg" {
+  name = "kaioc-terraform-sg"
+  vpc_id = "vpc-faf77d9d"
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["172.20.178.0/23","15.0.0.0/9","172.20.168.0/22"] # hp subnets + hp vpn
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["172.20.178.0/23","15.0.0.0/9","172.20.168.0/22"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["172.20.178.0/23","15.0.0.0/9","172.20.168.0/22"]
+  }
+  egress {
+    from_port = -1
+    to_port = -1
+    protocol = "all"
+    cidr_blocks = ["0.0.0.0/0"] # inside out
+  }
+  tags = {
+    Name = "kaioc-terraform-sg"
+  }
+}
---
+++ test/marcos.tf
@@ -0,0 +1,51 @@
+# provider "aws" {
+# region = "us-east-1"
+# }
+
+
+
+# resource "aws_instance" "felipe" {
+# ami = "ami-02e136e904f3da870"
+# instance_type = "t2.micro"
+# vpc_security_group_ids = [aws_security_group.kaio.id]
+# subnet_id= "subnet-5175a71b"
+# }
+
+
+
+# resource "aws_security_group" "kaio" {
+
+
+
+# name = "terraform-test-instance"
+# vpc_id = "vpc-f7138f91"
+
+
+
+# ingress{
+# from_port = 80
+# to_port = 80
+# protocol = "tcp"
+# cidr_blocks = ["0.0.0.0/0"]
+# }
+# }
+
+
+# provider "aws" {
+#   region = "us-west-2"
+# }
+
+resource "aws_instance" "robertom-terraform-ec2-example" {
+  ami = "ami-0c2d06d50ce30b442"
+  instance_type = "t2.micro"
+  key_name= "robertom-terraform-test"
+  #  subnet_id = "subnet-5f359a38" # PRIVATE-AZ-2A
+  #  subnet_id = "subnet-4a32f303" # PRIVATE-AZ-2B
+  subnet_id = "subnet-c2e2cd9a" # PRIVATE-AZ-2C
+
+  vpc_security_group_ids = [aws_security_group.robertom-terraform-sg-example.id]
+  iam_instance_profile = "PingInstanceRole"
+  user_data = file("./scripts/install_nginx.sh")
+  tags = {
+    Name = "robertom-terraform-ec2-example"
+  }
